91177f07d6bf0a026cf5e583692d51aa5c16072f
vagrant <vagrant@localhost.localdomain>
2021-01-18 10:46:09 +0000

    loginButton設置

--- Swift5Keyboard1/ViewController.swift
+++ Swift5Keyboard1/ViewController.swift
@@ -7,13 +7,52 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    
+    @IBOutlet weak var logoImageView: UIImageView!
+    
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    
+    
+    @IBOutlet weak var passWordTextField: UITextField!
+    
+    
+    @IBOutlet weak var userNameLabel: UILabel!
+    
+    
+    @IBOutlet weak var passwordLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameTextField.delegate = self
+        passWordTextField.delegate = self
+        
+        
+        
     }
 
 
-}
+    @IBAction func login(_ sender: Any) {
+        
+        logoImageView.image = UIImage(named: "loginOK")
+        
+        userNameLabel.text = userNameTextField.text
+        passwordLabel.text = passWordTextField.text
+        
+        //画面タッチでキーボードを閉じる
+        
+        }
+        
+        //リターンキーを押した時にキーボードを閉じる
+        func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+            //キーボードが閉じる
+            textField.resignFirstResponder()
+            return true
+        }
+        
+    }
+    
 
